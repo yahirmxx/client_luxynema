8d762421dcf6951725c20b07a94653e4a91341f3
Francisco Ramos <franciscoramos0709@gmail.com>
2023-12-06 19:08:17 -0700

    nuevos cambios 061223
    nuevos cambios de antigua rama de git

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-
+import "./Movies.css";
 export const Movies = () => {
   return (
   <div>
 
-      <section className="sm: flex-wrap bg-white mx-20 rounded-xl mt-40 ">
-        <div className="sm: Contenedor flex flex-wrap "> 
-          <img className="moviePicture w-72  p-4 my-6" src="../src/assets/arrival.jpg" />
-          <div className="contenido ">
+      <section className="flex justify-center align-center sm:flex-wrap md:flex-wrap  md:p-0  bg-white mx-40 xl:mx-40 rounded-xl mt-40 ">
+        <div className="border-8 flex   p-0">
+        <div className="justify-between align-center ">
+        <img className="justify-self-center self-center mt-10 mb-10 h-96 border-8 w-96 " src="../src/assets/arrival.jpg" />
+        </div>
+          <div className="contenido justify-between ">
             <h2
               id="horarios"
               className="uppercase text-2xl font-medium lemon-milk"
@@ -29,25 +31,132 @@ export const Movies = () => {
               12pm
             </a>
             <hr className="bg-[color:var(--negro)] w-100 h-1"></hr>
-            <div className="flex flex-wrap">
-            <h3 >asientos</h3>
+            <div className="flex flex-wrap ">           
+
+            <div className="body">
+            <h1>asientos</h1>
+                        <div className="movie-container">
+                                  <label>Pick a movie:</label>
+                                  <select id="movie">
+                                    <option value="10">La llegada ($10)</option>
+                                    <option value="12">El Padrino ($12)</option>
+                                    <option value="8">Buenos Muchachos ($8)</option>
+                                    <option value="9">Bastardos sin gloria ($9)</option>
+                                  </select>
+                                </div>
+
+                                <ul className="showcase">
+                                  <li>
+                                    <div className="seat"></div>
+                                    <small>N/A</small>
+                                  </li>
+                                  <li>
+                                    <div className="seat selected"></div>
+                                    <small>Selected</small>
+                                  </li>
+                                  <li>
+                                    <div className="seat occupied"></div>
+                                    <small>Occupied</small>
+                                  </li>
+                                </ul>
+
+                                <div className="container">
+                                  <div className="screen"></div>
+                                  <div className="row">
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                  </div>
+
+                                  <div className="row">
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat occupied"></div>
+                                    <div className="seat occupied"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                  </div>
+
+                                  <div className="row">
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat occupied"></div>
+                                    <div className="seat occupied"></div>
+                                  </div>
+
+                                  <div className="row">
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                  </div>
+
+                                  <div className="row">
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat occupied"></div>
+                                    <div className="seat occupied"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                  </div>
+
+                                  <div className="row">
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat"></div>
+                                    <div className="seat occupied"></div>
+                                    <div className="seat occupied"></div>
+                                    <div className="seat occupied"></div>
+                                    <div className="seat"></div>
+                                  </div>
+                                </div>
+
+                                <p className="text">
+                                  You have selected <span id="count">0</span> seats for a price of $<span
+                                    id="total"
+                                    >0</span>
+                                </p>
+                        
+                      </div>
+
+                      
+
+
           
-            <div className="contenidoCheckOut flex-wrap bg-gray-300  rounded-xl mt-4">
+            <div className="contenidoCheckOut w-fit h-fit flex-wrap bg-gray-300  rounded-xl mt-4">
               <div className="innerCheckOut flex flex-wrap ">
                 <img
                   className="moviePictureCheckOut w-40 p-2"
                   src="../src/assets/arrival.jpg"
                 />
                 <div className="box  ">
-                  <h3 className="uppercase text-2xl font-medium lemon-milk">
+                  <h3 className="uppercase text-2xl font-medium lemon-milk mt-20">
                     CheckOut
                   </h3>
-                  <p>total: 0.00.0.0$</p>
+                  <p>total:  $</p>
                   <p>hora: 00:00:00:00</p>
                   <p>asientos: A0,B0,C0</p>
                 </div>
               </div>
-              <div className="ml-10">
+              <div className="ml-4">
               <p>Arrival</p>
               <p>1h 56m</p>
               <a
@@ -61,14 +170,11 @@ export const Movies = () => {
             </div>
             </div>
           </div>
-        </div>
-        <a
-          className="trailer"
-          href="https://www.youtube.com/watch?v=uWs5lsWXLbo&ab_channel=SonyPicturesEspa%C3%B1a"
-        >
-          watch trailer
-        </a>
+        
+          </div>
       </section>
 
-  </div>);
+  </div>
+  
+  );
 };
